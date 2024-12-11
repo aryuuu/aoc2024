fc3c6115137ea6c64ef0328b1320287829fc9314
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-12-11 19:27:16 +0700

    feat: day 9 part 1

--- build.zig
+++ build.zig
@@ -162,6 +162,21 @@ pub fn build(b: *std.Build) void {
     const run_day8_step = b.step("day8", "Run the app");
     run_day8_step.dependOn(&run_day8_cmd.step);
 
+    const day9_exe = b.addExecutable(.{
+        .name = "aoc2029_day9",
+        .root_source_file = b.path("src/day9.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(day9_exe);
+    const run_day9_cmd = b.addRunArtifact(day9_exe);
+    run_day9_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_day9_cmd.addArgs(args);
+    }
+    const run_day9_step = b.step("day9", "Run the app");
+    run_day9_step.dependOn(&run_day9_cmd.step);
+
     // TESTS
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -226,6 +241,13 @@ pub fn build(b: *std.Build) void {
     });
     const run_day8_unit_tests = b.addRunArtifact(day8_unit_tests);
 
+    const day9_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/day9.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_day9_unit_tests = b.addRunArtifact(day9_unit_tests);
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
@@ -238,6 +260,7 @@ pub fn build(b: *std.Build) void {
     const test_day6_step = b.step("test_day6", "Run unit tests for day6");
     const test_day7_step = b.step("test_day7", "Run unit tests for day7");
     const test_day8_step = b.step("test_day8", "Run unit tests for day8");
+    const test_day9_step = b.step("test_day9", "Run unit tests for day9");
     test_step.dependOn(&run_exe_unit_tests.step);
     test_day1_step.dependOn(&run_day1_unit_tests.step);
     test_day2_step.dependOn(&run_day2_unit_tests.step);
@@ -247,4 +270,5 @@ pub fn build(b: *std.Build) void {
     test_day6_step.dependOn(&run_day6_unit_tests.step);
     test_day7_step.dependOn(&run_day7_unit_tests.step);
     test_day8_step.dependOn(&run_day8_unit_tests.step);
+    test_day9_step.dependOn(&run_day9_unit_tests.step);
 }
---
+++ src/day9.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const result = try part1(allocator, "./input/day9.txt");
+    std.debug.print("result: {d}\n", .{result});
+}
+
+fn part1(allocator: std.mem.Allocator, filename: []const u8) !usize {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+
+    var read_buffer = std.io.bufferedReader(file.reader());
+    var reader = read_buffer.reader();
+
+    var blocks = std.ArrayList(Block).init(allocator);
+    defer blocks.deinit();
+
+    var curr_id: usize = 0;
+    var is_free = false;
+    var buf: [1024]u8 = undefined;
+    while (true) {
+        const read_len = try reader.read(&buf);
+        if (read_len == 0) {
+            break;
+        }
+
+        for (buf) |char| {
+            if (char < '0') {
+                break;
+            }
+            const num = char - '0';
+            if (is_free) {
+                for (0..num) |_| {
+                    try blocks.append(Block.free);
+                }
+            } else {
+                for (0..num) |_| {
+                    try blocks.append(Block{ .file = curr_id });
+                }
+                curr_id += 1;
+            }
+            is_free = !is_free;
+        }
+    }
+
+    var i: usize = 0;
+    var j: usize = blocks.items.len - 1;
+
+    while (i < j) {
+        const head = blocks.items[i];
+        const tail = blocks.items[j];
+
+        switch (head) {
+            Block.file => |_| {
+                i += 1;
+
+                switch (tail) {
+                    Block.free => j -= 1,
+                    else => {},
+                }
+            },
+            Block.free => {
+                switch (tail) {
+                    Block.free => j -= 1,
+                    Block.file => |id| {
+                        blocks.items[i] = Block{ .file = id };
+                        blocks.items[j] = Block.free;
+                        i += 1;
+                        j -= 1;
+                    },
+                }
+            },
+        }
+    }
+
+    var total: usize = 0;
+    for (blocks.items, 0..) |item, idx| {
+        switch (item) {
+            Block.file => |id| {
+                total += idx * id;
+            },
+            else => break,
+        }
+    }
+
+    return total;
+}
+
+const Block = union(enum) {
+    free,
+    file: usize,
+};
+
+test "part 1" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const result = try part1(allocator, "./input/day9.test.txt");
+    try std.testing.expectEqual(1928, result);
+}
